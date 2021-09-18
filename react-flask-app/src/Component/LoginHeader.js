aa8f8c8291a464ce027431dc0bc5214a3585f4a8
Billy Kim <1kimbil0305@gmail.com>
2021-09-18 16:37:24 -0400

    separated sign up and login

--- react-flask-app/src/Component/LoginHeader.js
+++ react-flask-app/src/Component/LoginHeader.js
@@ -3,15 +3,26 @@ import { useState } from "react"
 
 const LoginHeader = () => {
     const [notEntered, setnotEntered] = useState(true)
+    const [signIn, setSignIn] = useState(false)
+
+    const onClick = () => {
+        setSignIn(true)
+    }
     return (
         <div className="login-container">
-            {notEntered ? 
-            <div className="signup-header">
-                <h1>Create account</h1>
-                <h2>Already have an account? Sign in</h2>
-            </div> 
-            : <h1>Successfully entered</h1>}
-            {notEntered && <Credentials notEntered={notEntered} setnotEntered={setnotEntered}/>}
+
+            {notEntered ?
+                (signIn ? 
+                <h1>sign in please</h1> 
+                : 
+                <div className="signup-header">
+                    <h1>Create account</h1>
+                    <h2>Already have an account? </h2> <h2 onClick={onClick} style={{cursor: 'pointer'}}>Sign in</h2>
+                    <Credentials notEntered={notEntered} setnotEntered={setnotEntered}/>
+                </div>)
+                :
+                <h1>Successfully entered</h1>
+            }
         </div>
     )
 }
